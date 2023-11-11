175c4c283867cb98fbde22430971098dc9f40ca7
justians <117990728+justians@users.noreply.github.com>
2023-11-11 02:37:35 +0000

    Update balls.py

--- balls.py
+++ balls.py
@@ -1,18 +1,41 @@
 from selenium import webdriver
 import time
 from bs4 import BeautifulSoup
-import requests
-driver = webdriver.Chrome()
-url = "https://www.google.com/search?p=trees"
-driver.maximize_window()
-driver.get(url)
+from selenium.webdriver.chrome.options import Options
 
-page = driver.page_source.encode('utf-8').strip()
+dict = {}
+counter = 0
+options = webdriver.ChromeOptions()
+options.add_argument('--blink-settings=imagesEnabled=false')
+options.add_argument('headless')
+#global driver
+driver = webdriver.Chrome(options=options)
 
-soup = BeautifulSoup(page, 'html.parser')
+def search():
+    global driver
+    try: 
+        url= f'https://www.google.com/search?q={line}'
+        driver.set_window_size(100,100)
+        driver.get(url)
+        page = driver.page_source.encode('utf-8').strip()
+        soup = BeautifulSoup(page, "html.parser")
+        combien = soup.find("div", {"id":"result-stats"}).text
+        combien = int(combien[6:-24].translate({ord(','):None}))
+        dict.update({line: combien})
+        global counter 
+        counter += 1
+        print(f"Pages scanned: {counter}. . .")
+    except:
+        driver.quit()
+        driver = webdriver.Chrome(options=options)
+        
+with open(r'C:\Users\iansp\Downloads\words.txt', 'r') as f:
+# Do something with the file
+    lines = f.readlines()
 
-combien = soup.find('div', {'id': 'result-stats'}).text
+for line in lines:
+    search()
+    
 
-print(int(combien[6:-24].translate({ord(','): None})))
-
+print(dict)
 driver.quit()
